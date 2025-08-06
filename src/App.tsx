07783e99838974f46d12a83ea023945a1a60070f
조하은 <jhe93@naver.com>
2025-08-06 20:30:07 +0900

    refactor: 라우터 로직 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,12 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import RootLayout from './pages/Root';
-
-import ErrorPage from './pages/Error';
-import MainPage from './pages/MainPage';
-import GithubPage from './pages/GithubPage';
-import Greeting from './pages/Greeting';
-import CreateGroupPage from './pages/CreateGroupPage';
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <RootLayout />,
-        errorElement: <ErrorPage />,
-        children: [
-            { index: true, element: <MainPage /> },
-            {
-                path: 'github',
-                element: <GithubPage />,
-            },
-            {
-                path: 'create',
-                element: <CreateGroupPage />,
-            },
-            {
-                path: 'greeting',
-                element: <Greeting />,
-            },
-        ],
-    },
-]);
+import { RouterProvider } from 'react-router-dom';
+import { AppProviders } from './providers';
+import { router } from './router';
 
 function App() {
-    return <RouterProvider router={router} />;
+    return (
+        <AppProviders>
+            <RouterProvider router={router} />
+        </AppProviders>
+    );
 }
 export default App;
